39b35e42ae709e9731feba4359fc740ed02e3cd0
Gauri Gattani <gaurisoft@gmail.com>
2021-08-20 11:50:30 -0400

    create table to represent teams

--- src/components/Teams.tsx
+++ src/components/Teams.tsx
@@ -1,70 +1,57 @@
 import React, { useReducer } from "react";
 import useGetTeams from "../hooks/getTeams";
-import InfiniteScroll from "../hooks/InfiniteScroll";
-import useGetTeamMembers from "../hooks/getTeamMembers";
 import teamsReducer from "../reducers/TeamsReducer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Table } from "react-bootstrap";
 
 function Teams() {
   const [state, dispatch] = useReducer(teamsReducer, []);
+  const { loading } = useGetTeams(dispatch);
 
-  const { loading: teamsLoading } = useGetTeams(dispatch);
-  const { loading: teamMembersLoading } = useGetTeamMembers("2097", dispatch);
-
-  let hasMoreData = true;
-  const loadMoreNumbers = () => {
-    console.log("fetching");
-  };
-
-  const getTeamMembers = async (teamId) => {
-    try {
-      console.log("team members fetching");
-      const response = await fetch(
-        `https://statsapi.mlb.com/api/v1/teams/${teamId}/roster?rosterType=active`
-      );
-      const data = await response.json();
-      dispatch({ type: "get_team_members_data", payload: data });
-    } catch (error) {
-      //setError("There was an error while getting team members");
-    }
-  };
-
-  const loading = teamsLoading || teamMembersLoading;
+  const getTeamMembers = (teamId) => {};
 
   if (loading) {
     return <div>"Loading..."</div>;
   }
 
   return (
-    <InfiniteScroll
-      hasMoreData={hasMoreData}
-      isLoading={loading}
-      onBottomHit={loadMoreNumbers}
-      loadOnMount={true}
-    >
-      <ol style={{ height: window.innerHeight }}>
+    <Table hover variant="dark">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Team</th>
+          <th>League</th>
+        </tr>
+      </thead>
+      <tbody>
         {state.map((team, i) => (
-          <li key={`${team.id}${team.teamCode}`}>
-            <div>
-              <button
+          <tr>
+            <td className={`align-items-center`}>{`${i + 1}`}</td>
+            <td>
+              <Button
+                variant="link"
+                style={{ color: "#FFF", textDecoration: "none" }}
                 onClick={() => {
                   getTeamMembers(team.id);
                 }}
               >
                 {team.name}
-              </button>
-              <ul>
+              </Button>
+            </td>
+            <td>{team.league.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+
+export default Teams;
+
+/*  <ul>
                 {team.members?.map((teamMember) => (
                   <li key={`${teamMember.parentTeamId}${teamMember.person.id}`}>
                     {teamMember.person.fullName}
                   </li>
                 ))}
-              </ul>
-            </div>
-          </li>
-        ))}
-      </ol>
-    </InfiniteScroll>
-  );
-}
-
-export default Teams;
+              </ul>*/
