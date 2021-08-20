de04ed426bb36c4b49f5c79cb719e06c3c4e1a31
Gauri Gattani <gaurisoft@gmail.com>
2021-08-20 08:33:09 -0400

    fetch team members on each button click

--- src/components/Teams.tsx
+++ src/components/Teams.tsx
@@ -7,18 +7,32 @@ import teamsReducer from "../reducers/TeamsReducer";
 function Teams() {
   const [state, dispatch] = useReducer(teamsReducer, []);
 
-  const { loading: teamsLoading, teams } = useGetTeams(dispatch);
-  const { loading: teamMembersLoading, teamMembers } = useGetTeamMembers(
-    "2097",
-    dispatch
-  );
+  const { loading: teamsLoading } = useGetTeams(dispatch);
+  const { loading: teamMembersLoading } = useGetTeamMembers("2097", dispatch);
 
   let hasMoreData = true;
   const loadMoreNumbers = () => {
     console.log("fetching");
   };
 
+  const getTeamMembers = async (teamId) => {
+    try {
+      console.log("team members fetching");
+      const response = await fetch(
+        `https://statsapi.mlb.com/api/v1/teams/${teamId}/roster?rosterType=active`
+      );
+      const data = await response.json();
+      dispatch({ type: "get_team_members_data", payload: data });
+    } catch (error) {
+      //setError("There was an error while getting team members");
+    }
+  };
+
   const loading = teamsLoading || teamMembersLoading;
+
+  if (loading) {
+    return <div>"Loading..."</div>;
+  }
 
   return (
     <InfiniteScroll
@@ -27,12 +41,17 @@ function Teams() {
       onBottomHit={loadMoreNumbers}
       loadOnMount={true}
     >
-      {loading && <div>"Loading..."</div>}
       <ol style={{ height: window.innerHeight }}>
         {state.map((team, i) => (
           <li key={`${team.id}${team.teamCode}`}>
             <div>
-              {team.name}
+              <button
+                onClick={() => {
+                  getTeamMembers(team.id);
+                }}
+              >
+                {team.name}
+              </button>
               <ul>
                 {team.members?.map((teamMember) => (
                   <li key={`${teamMember.parentTeamId}${teamMember.person.id}`}>
